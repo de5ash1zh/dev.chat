cdf9a963bd16c19a4d08f6186973e31ea1889579
Devashish <dev@Devashishs-MacBook-Air.local>
2025-02-07 11:49:12 +0530

    setup database connection

--- backend/eslint.config.js
+++ backend/eslint.config.js
@@ -1,8 +1,19 @@
-import globals from 'globals';
 import pluginJs from '@eslint/js';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import globals from 'globals';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { languageOptions: { globals: globals.node } },
+  {
+    languageOptions: { globals: globals.node },
+    plugins: {
+      'simple-import-sort': simpleImportSort
+    },
+    rules: {
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error'
+    }
+  },
+
   pluginJs.configs.recommended
 ];
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,12 @@
 import express from 'express';
 const app = express();
 import { StatusCodes } from 'http-status-codes';
+
+import connectDB from './config/dbConfig.js';
 import { PORT } from './config/serverConfig.js';
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/ping', (req, res) => {
   return res.status(StatusCodes.OK).json({ message: 'pong' });
@@ -9,4 +14,5 @@ app.get('/ping', (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`Server is running on the port ${PORT}`);
+  connectDB();
 });
