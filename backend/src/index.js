0dd8a0568236abeea93b74e3418e77cc16688c38
Devashish <dev@Devashishs-MacBook-Air.local>
2025-02-07 10:51:59 +0530

    setup http status codes

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,12 @@
 import express from "express";
 const app = express();
-import { PORT } from "./config/serverConfig.js"; 
+import { StatusCodes } from "http-status-codes";
+import { PORT } from "./config/serverConfig.js";
 
 app.get("/ping", (req, res) => {
-  return res.status(200).json({ message: "pong" });
+  return res.status(StatusCodes.OK).json({ message: "pong" });
 });
 
-app.listen(PORT,()=>{
-    console.log(`Server is running on the port ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`Server is running on the port ${PORT}`);
+});
