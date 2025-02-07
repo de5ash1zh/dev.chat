235e2fdca98d5dca984044516e7dde8b40495aa1
Devashish <dev@Devashishs-MacBook-Air.local>
2025-02-07 14:21:57 +0530

    added user schema with robohash for DP

--- backend/src/routes/v1/users.js
+++ backend/src/routes/v1/users.js
@@ -1,7 +1,7 @@
 import express from 'express';
 const router = express.Router();
 
-router.get('/users', (req, res) => {
+router.get('/', (req, res) => {
   return res.status(200).json({
     message: 'GET /users'
   });
